41cdf7d7c5bbd9b999558af29392ebb469ae8050
Ram-N <ramnarasimhan@gmail.com>
2014-01-15 15:59:33 -0800

    Added tryCatch for readURL

--- R/weatherData_lib.R
+++ R/weatherData_lib.R
@@ -18,7 +18,7 @@ IsDateInvalid <- function (date) {
   return(0) #is okay
 }
 
-
+#' @export
 IsStateTypeInvalid <- function (station_type) {
     if(station_type != "airportcode" && 
          station_type != "id")  {
@@ -28,6 +28,54 @@ IsStateTypeInvalid <- function (station_type) {
     return(0) #is okay
   }
   
+
+
+#----------------------------------------------------------------
+#function to read URL with tryCatch
+#' @return NA if unsuccessful
+readUrl <- function(final_url) {
+  out <- tryCatch(
+{
+  #if you want to use more than one R expression in the "try" part use {}
+  # 'tryCatch()' will return the last evaluated expression 
+  # in case the "try" part was completed successfully  
+  #message("This is the 'try' part")  
+  u <- url(final_url)  
+  readLines(u, warn=FALSE) 
+  # The return value of `readLines()` is the actual value 
+  # that will be returned in case there is no condition 
+  # (e.g. warning or error). 
+  # You don't need to state the return value via `return()` as code 
+  # in the "try" part is not wrapped insided a function (unlike that
+  # for the condition handlers for warnings and error below)
+},
+error=function(cond) {
+  message(paste("URL does not seem to exist:", final_url))
+  message("The original error message:")
+  message(cond)
+  return(NA)  # Choose a return value in case of error
+},
+warning=function(cond) {
+  message(paste("URL caused a warning:", final_url))
+  message("Here's the original warning message:")
+  message(cond)
+  # Choose a return value in case of warning
+  return(NA)
+},
+ finally=  close(u)
+#   # NOTE:
+#   # Here goes everything that should be executed at the end,
+#   # regardless of success or error.
+#   # If you want more than one expression to be executed, then you 
+#   # need to wrap them in curly brackets ({...}); otherwise you could
+#   # just have written 'finally=<expression>' 
+#   message(paste("Processed URL:", url))
+#   message("Some other message at the end")
+# }
+  )    
+return(out)
+}
+#----------------------------------------------------------------
 
 #' This function gets data from Wx Underground, given a valid station and date
 #' TODO: Error checking for valid station name, date, station.type
@@ -61,83 +109,90 @@ FetchDailyWeatherForStation <- function(station,
   # Type can be ID OR Airport  
   if(station_type=="id") {
     base_url <- 'http://www.wunderground.com/weatherstation/WXDailyHistory.asp?'
-    final_url <- paste(base_url,
+    final_url <- paste0(base_url,
                        'ID=', station,
                        '&month=', m,
                        '&day=', d, 
                        '&year=', y,
-                       '&format=1', sep='')    
+                       '&format=1')    
   }
   
   #for airport codes
   if(station_type=="airportcode") {
     airp_url = 'http://www.wunderground.com/history/airport/K'
-    coda = '/DailyHistory.html?format=1'
-    
-    final_url <- paste(airp_url, station,
+    coda = '/DailyHistory.html?format=1'    
+    final_url <- paste0(airp_url, station,
                      '/',y,
                      '/',m,
                      '/',d,
-                     coda, sep='')
-  }  
+                     coda)
+  }    
+  if(opt.debug) print(final_url)
+  
+  #------------------
+  # Now read the data from the URL
+  #-------------------
   
   # reading in as raw lines from the web server
   # contains <br> tags on every other line
-  if(opt.debug) print(final_url)
-  
-  u <- url(final_url)
-  the_data <- readLines(u) #the_data is not a chr vector, each line = 1 element of the vector
-  close(u)
-    
+#  u <- url(final_url)
+  raw_data <- readUrl(final_url)
+  if(is.na(raw_data)) return(NULL) #have to try again
+  # print(is.na(raw_data))
+  # raw_data <- readLines(u) #raw_data is not a chr vector, each line = 1 element of the vector
+  # close(u)
+
   # only keep records with more than 5 rows of data
-  if(length(the_data) < 5 ) {
-    print(paste("not enough records found for", station))
+  if(length(raw_data) < 5 ) {
+    print(paste("not enough records found for", station, 
+                "/n Only", length(raw_data), "records."))
     return(NULL)
   }
   # remove the first line
-  the_data <- the_data[-c(1)]    
+  raw_data <- raw_data[-c(1)]    
 
   #remove the last line
-  #the_data <- the_data[-c(length(the_data))]    
+  #raw_data <- raw_data[-c(length(raw_data))]    
   
   if(opt.compress.output==TRUE) {
     # remove odd numbers starting from 3 --> end
-    the_data <- the_data[-seq(3, length(the_data), by=2)]    
+    raw_data <- raw_data[-seq(3, length(raw_data), by=2)]    
   }
 
-  the_data <- gsub("<br />", "", the_data) #get rid of the trailing br at the end of each line
-  
+  raw_data <- gsub("<br />", "", raw_data) #get rid of the trailing br at the end of each line
   
   # extract header and cleanup
-  the_header <- the_data[1]
+  the_header <- raw_data[1]
+  if(opt.debug ==TRUE)
+    print(the_header)
+  
+  # convert to CSV, without header
+  tC <- textConnection(paste(raw_data, collapse='\n'))
+  raw_data <- read.csv(tC, as.is=TRUE, row.names=NULL, header=FALSE, skip=1) #makes it a data.frame
+  close(tC)
+  
   the_header <- make.names(strsplit(the_header, ',')[[1]]) #make the col.names syntactically valid
   #     print(the_header)
   #     print(length(the_header))
-  
-  # convert to CSV, without header
-  tC <- textConnection(paste(the_data, collapse='\n'))
-  the_data <- read.csv(tC, as.is=TRUE, row.names=NULL, header=FALSE, skip=1) #makes it a data.frame
-  close(tC)
-  
   # assign column names
-  names(the_data) <- the_header
+  names(raw_data) <- the_header
   
   # convert Time column into properly encoded date time
-  the_data$Time <- as.POSIXct(strptime(paste(date,the_data[[1]]), format='%Y-%m-%d %I:%M %p'))
+  raw_data$Time <- as.POSIXct(strptime(paste(date,raw_data[[1]]), format='%Y-%m-%d %I:%M %p'))
   
   # remove UTC and software type columns
-  the_data$DateUTC.br. <- NULL
-  the_data$SoftwareType <- NULL
+  raw_data$DateUTC.br. <- NULL
+  raw_data$SoftwareType <- NULL
   
   # sort and fix rownames
-  the_data <- the_data[order(the_data$Time), ] #sort the rows by increasing time
-  row.names(the_data) <- 1:nrow(the_data) #give the dataframe rownames
+  raw_data <- raw_data[order(raw_data$Time), ] #sort the rows by increasing time
+  row.names(raw_data) <- 1:nrow(raw_data) #give the dataframe rownames
   
-  df <- the_data
+  df <- raw_data
   if(opt.temp.only) {
-    df <- the_data[c("Time", "TemperatureF")]
+    df <- raw_data[c("Time", "TemperatureF")]
   }
-  # done
+  
   return(df)
 }
 
@@ -153,8 +208,10 @@ FetchDailyWeatherForStation <- function(station,
 #' 
 #' 
 IsStationDataAvailable<- function (station, station.type, start_date, end_date) {
-  lst.start<- FetchDailyWeatherForStation(station, start_date, station.type, opt.temp.only=T, opt.compress.output=T)
-  lst.end<- FetchDailyWeatherForStation(station, end_date, station.type, opt.temp.only=T, opt.compress.output=T)
+  lst.start<- FetchDailyWeatherForStation(station, start_date, station.type, 
+                                          opt.temp.only=T, opt.compress.output=T, opt.debug=T)
+  lst.end<- FetchDailyWeatherForStation(station, end_date, station.type, opt.temp.only=T, 
+                                        opt.compress.output=T, opt.debug=T)
   st.row = nrow(lst.start) #takes on a value of NULL if station has no data
   en.row = nrow(lst.end)
   
@@ -176,12 +233,9 @@ IsStationDataAvailable<- function (station, station.type, start_date, end_date) 
 #'  @author Ram Narasimhan
 #'  
 #' @param station is a valid 3-letter airport code or a valid Weather Station ID
-#' 
 #' @param start_date is a valid string representing a date in the past (YYYY-MM-DD, all numeric)
-#' 
-#' @param end_date is a a valid string representing a date in the past (YYYY-MM-DD, all numeric) and is greater than start_date
-#'  
-#'  @param station.type = "airportCode" (3-letter airport code) or "ID" (Wx call Sign)
+#' @param end_date is a a valid string representing a date in the past (YYYY-MM-DD, all numeric) and is greater than start_date  
+#' @param station.type = "airportCode" (3-letter airport code) or "ID" (Wx call Sign)
 
 FetchStationWeatherForDateRange <- function(station, station.type, start.date, end.date) {
   
@@ -199,15 +253,15 @@ FetchStationWeatherForDateRange <- function(station, station.type, start.date, e
   # loop over dates, and fetch data
   for(i in seq_along(date.range))
   {
-    print(paste(station, i, date.range[i]))    
     l[[i]] <- FetchDailyWeatherForStation(station, date.range[i], station.type)
+    print(paste(station, i, date.range[i], ":",  nrow(l[[i]]) ))
   }
   
   #print(l)  
   # stack elements of list into DF, filling missing columns with NA
   d <- ldply(l)
     
-  outFileName = paste(station, "csv","gz", sep='.')
+  outFileName = paste(station,"_",start.date,"_",end.date, "csv","gz", sep='.')
   # save to CSV
   write.csv(d, file=gzfile(outFileName), row.names=FALSE)
   print(paste("wrote:", outFileName, "to", getwd()))
