2c0d41126eb0d95b4a514bdb8236c2b23a4b5982
Ram-N <ramnarasimhan@gmail.com>
2014-01-26 21:29:24 -0800

    Updated Documentation + Mac Check

---
+++ R/data_description.R
@@ -0,0 +1,75 @@
+#' @title Data - Ambient Temperature for the City of London for all of 2013
+#'
+#' @description This is a data frame of Ambient temperature data, extracted from Weather Undergound.
+#' Each row has two entries (columns). The Timestamp (YYYY-MM-DD HH:MM:SS) and the Temperature (in degrees F)
+#'
+#' @author Ram Narasimhan \email{ramnarasimhan@@gmail.com}
+#' @name London2013
+#' @docType data
+#' @usage data(London2013)
+#' @references 
+#' \url{http://www.wunderground.com/history/airport/EGLL/2013/1/1/DailyHistory.html?format=1}
+#' @keywords data
+#' 
+NULL
+
+#' @title Data - Ambient Temperature for the City of Mumbai, India for all of 2013
+#'
+#' @description This is a data frame of Ambient temperature data, extracted from Weather Undergound.
+#' Each row has two entries (columns). The Timestamp (YYYY-MM-DD HH:MM:SS) and the Temperature (in degrees F)
+#'
+#' @author Ram Narasimhan \email{ramnarasimhan@@gmail.com}
+#' @name Mumbai2013
+#' @docType data
+#' @usage data(Mumbai2013)
+#' @references 
+#' \url{http://www.wunderground.com/history/airport/VABB/2014/1/1/DailyHistory.html?format=1}
+#' @keywords data
+#' 
+NULL
+
+#' @title Data - Ambient Temperature for New York City for all of 2013
+#'
+#' @description This is a data frame of Ambient temperature data, extracted from Weather Undergound.
+#' Each row has two entries (columns). The Timestamp (YYYY-MM-DD HH:MM:SS) and the Temperature (in degrees F)
+#'
+#' @author Ram Narasimhan \email{ramnarasimhan@@gmail.com}
+#' @name NewYork2013
+#' @docType data
+#' @usage data(NewYork2013)
+#' @references 
+#' \url{http://www.wunderground.com/history/airport/KLGA/2013/1/1/DailyHistory.html?format=1}
+#' @keywords data
+#' 
+NULL
+
+#' @title Data - Ambient Temperature for the City of San Francisco for all of 2013
+#'
+#' @description This is a data frame of Ambient temperature data, extracted from Weather Undergound.
+#' Each row has two entries (columns). The Timestamp (YYYY-MM-DD HH:MM:SS) and the Temperature (in degrees F)
+#'
+#' @author Ram Narasimhan \email{ramnarasimhan@@gmail.com}
+#' @name SFO2013
+#' @docType data
+#' @usage data(SFO2013)
+#' @references 
+#' \url{http://www.wunderground.com/history/airport/KSFO/2013/1/1/DailyHistory.html?format=1}
+#' @keywords data
+#' 
+NULL
+
+#' @title Data - Ambient Temperature for the City of San Francisco for all of 2012
+#'
+#' @description This is a data frame of Ambient temperature data, extracted from Weather Undergound.
+#' Each row has two entries (columns). The Timestamp (YYYY-MM-DD HH:MM:SS) and the Temperature (in degrees F)
+#'
+#' @author Ram Narasimhan \email{ramnarasimhan@@gmail.com}
+#' @name SFO2012
+#' @docType data
+#' @usage data(SFO2012)
+#' @references 
+#' \url{http://www.wunderground.com/history/airport/KSFO/2012/1/1/DailyHistory.html?format=1}
+#' @keywords data
+#' 
+NULL
+
--- R/weatherData_lib.R
+++ R/weatherData_lib.R
@@ -1,6 +1,5 @@
 require(plyr)
-#' 
-#' Internal check to see if the data format is valid
+
 IsDateInvalid <- function (date) {
   # d <- try( as.Date( date, format= "%d-%m-%Y %H:%M:%S" ) ) #original
   d <- try( as.Date( date) )
@@ -14,7 +13,8 @@ IsDateInvalid <- function (date) {
 #' @title Check if the station type is airportCode or id
 #' @description We are checking if a valid station type was given to the function.
 #' @param station_type can be \code{airportCode} which is the default, or it
-#'  can be \code{id}
+#'  can be \code{id} which is a weather-station ID
+#'  @seealso getStationCode
 IsStationTypeInvalid <- function (station_type) {
     if(station_type != "airportcode" && 
          station_type != "id")  {
@@ -33,7 +33,8 @@ readUrl <- function(final_url) {
   # in case the "try" part was completed successfully  
   #message("This is the 'try' part")  
   u <- url(final_url)  
-  readLines(u, warn=FALSE) 
+  # readLines(u, warn=FALSE) 
+  readLines(u) 
   # The return value of `readLines()` is the actual value 
   # that will be returned in case there is no condition 
   # (e.g. warning or error). 
@@ -69,27 +70,37 @@ return(out)
 }
 #----------------------------------------------------------------
 
-#' This function gets data from Wx Underground, given a valid station and date
+#' This function gets weather data, given a valid station and a single date
 #' 
 #' TODO: Error checking for valid station name, date, station.type
+#' 
+#' @param station is a valid 3-letter airport code or a valid Weather Station ID
+#' @param date is a valid string representing a date in the past (YYYY-MM-DD)
+ #' @param station_type can be \code{airportCode} which is the default, or it
+#'  can be \code{id} which is a weather-station ID
+
+#' @param opt_temperature_only is a flag to indicate only Temperature data is to be returned (default TRUE)
+#' @param opt.compress.output flag to indicate if a compressed output is preferred. If TRUE, only every other record is returned.
+#' @param opt_verbose A Flag to indicate if verbose output is desired
+#' 
+
 #' @export
-FetchDailyWeatherForStation <- function(station, 
-                                        date, 
-                                        station_type="airportCode",
-                                        opt.temp.only = T,
-                                        opt.compress.output = FALSE,
-                                        opt.debug = FALSE
-                                        ) {
+getSingleDayWeather <- function(station, 
+                                date, 
+                                station_type="airportCode",
+                                opt_temperature_only = T,
+                                opt.compress.output = FALSE,
+                                opt_verbose = FALSE) {
   
   # parse date  
   if(IsDateInvalid(date)) {
-    print("Date is Invalid")
+    warning(sprintf("Unable to build a valid URL \n Date format Invalid %s \n Input date should be within quotes \n and of the form 'YYYY-MM-DD' \n\n", date))     
     return(NULL)
   }
 
   station_type <- tolower(station_type)
   if(IsStationTypeInvalid(station_type)) {
-    print("Station Type is Invalid")
+    warning("Station Type is Invalid")
     return(NULL)
   }
   
@@ -127,7 +138,8 @@ FetchDailyWeatherForStation <- function(station,
                      '/',d,
                      coda)
   }    
-  if(opt.debug) print(final_url)
+  if(opt_verbose) 
+    cat(sprintf("Getting data from:\n %s\n",final_url))
   
   #------------------
   # Now read the data from the URL
@@ -137,14 +149,25 @@ FetchDailyWeatherForStation <- function(station,
   # contains <br> tags on every other line
 #  u <- url(final_url)
   raw_data <- readUrl(final_url)
-  if(is.na(raw_data)) return(NULL) #have to try again
-  # print(is.na(raw_data))
+  #print(str(raw_data))
+
+if(grepl(pattern="No daily or hourly history data", raw_data[3]) ==TRUE){
+  warning(sprintf("Unable to get data from URL \n Check Station name %s \n Inspect the validity of the URL being tried:\n %s \n", station, final_url))
+      return(NULL) #have to try again      
+}
+
+
+if(is.na(raw_data[1])) {
+  warning(sprintf("Unable to get data from URL\n Check if URL is reachable"))
+  return(NULL) #have to try again
+}
+# print(is.na(raw_data))
   # raw_data <- readLines(u) #raw_data is not a chr vector, each line = 1 element of the vector
   # close(u)
 
-  # only keep records with more than 5 rows of data
-  if(length(raw_data) < 5 ) {
-    print(paste("not enough records found for", station, 
+  # only keep records with more than 3 rows of data
+  if(length(raw_data) < 3 ) {
+    warning(paste("not enough records found for", station, 
                 "/n Only", length(raw_data), "records."))
     return(NULL)
   }
@@ -162,20 +185,20 @@ FetchDailyWeatherForStation <- function(station,
   raw_data <- gsub("<br />", "", raw_data) #get rid of the trailing br at the end of each line
   
   # extract header and cleanup
-  the_header <- raw_data[1]
-  if(opt.debug ==TRUE)
-    print(the_header)
+  header_row <- raw_data[1]
+#   if(opt_verbose ==TRUE)
+#     print(the_header)
   
   # convert to CSV, without header
   tC <- textConnection(paste(raw_data, collapse='\n'))
   raw_data <- read.csv(tC, as.is=TRUE, row.names=NULL, header=FALSE, skip=1) #makes it a data.frame
   close(tC)
   
-  the_header <- make.names(strsplit(the_header, ',')[[1]]) #make the col.names syntactically valid
+  header_row <- make.names(strsplit(header_row, ',')[[1]]) #make the col.names syntactically valid
   #     print(the_header)
   #     print(length(the_header))
   # assign column names
-  names(raw_data) <- the_header
+  names(raw_data) <- header_row
   
   # convert Time column into properly encoded date time
   raw_data$Time <- as.POSIXct(strptime(paste(date,raw_data[[1]]), format='%Y-%m-%d %I:%M %p'))
@@ -189,7 +212,7 @@ FetchDailyWeatherForStation <- function(station,
   row.names(raw_data) <- 1:nrow(raw_data) #give the dataframe rownames
   
   df <- raw_data
-  if(opt.temp.only) {
+  if(opt_temperature_only) {
     df <- raw_data[c("Time", "TemperatureF")]
   }
   
@@ -205,22 +228,24 @@ FetchDailyWeatherForStation <- function(station,
 #'  @details This functions checks for just the first and the last date in the interval
 #'    Not the days in between
 #' @param station is a valid 3-letter airport code or a valid Weather Station ID
-#' @param station.type is either \code{airportCode} or \code{id}
+#' @param station_type is either \code{airportCode} or \code{id}
 #' @param start_date is a valid string representing a date in the past (YYYY-MM-DD, all numeric)
 #' @param end_date is a a valid string representing a date in the past (YYYY-MM-DD, all numeric) and is greater than start_date
 #' 
 #' @export 
-IsStationDataAvailable<- function (station, station.type, start_date, end_date) {
-  lst.start<- FetchDailyWeatherForStation(station, start_date, station.type, 
-                                          opt.temp.only=T, opt.compress.output=T, opt.debug=T)
-  lst.end<- FetchDailyWeatherForStation(station, end_date, station.type, opt.temp.only=T, 
-                                        opt.compress.output=T, opt.debug=T)
-  st.row = nrow(lst.start) #takes on a value of NULL if station has no data
-  en.row = nrow(lst.end)
+IsStationDataAvailable<- function (station, station_type, start_date, end_date) {
+  lst_start <- getSingleDayWeather(station, start_date, station_type, 
+                                   opt_temperature_only=T, opt.compress.output=T, 
+                                   opt_verbose=T)
+  lst_end <- getSingleDayWeather(station, end_date, station_type,  opt_temperature_only=T, 
+                                        opt.compress.output=T, opt_verbose=T)
+  st_row = nrow(lst_start) #takes on a value of NULL if station has no data
+  en_row = nrow(lst_end)
   
-  cat(sprintf("For %s \n %d records for %s\n %d records for %s \n", station, st.row, start_date, en.row, end_date))
+  cat(sprintf("For %s \n Found %d records for %s\n Found %d records for %s \n",
+                station, st_row, start_date, en_row, end_date))
   
-  if (is.integer(st.row) && is.integer(en.row))
+  if (is.integer(st_row) && is.integer(en_row))
   {
     return(1)
   }
@@ -233,20 +258,22 @@ IsStationDataAvailable<- function (station, station.type, start_date, end_date) 
 #'  Getting data for full date-range
 #'  
 #' @details For each day in the date range, this function fetches Weather Data
+#' Internally, it makes multiple calls to getSingleDayWeather
+#' 
 #' @param station is a valid 3-letter airport code or a valid Weather Station ID
 #' @param start_date is a valid string representing a date in the past (YYYY-MM-DD, all numeric)
 #' @param end_date is a a valid string representing a date in the past (YYYY-MM-DD, all numeric) and is greater than start_date  
 #' @param station.type = "airportCode" (3-letter airport code) or "ID" (Wx call Sign)
 #' @export
-FetchStationWeatherForDateRange <- function(station, station.type, start.date, end.date) {
+getWeatherForDateRange <- function(station, station.type, start_date, end_date) {
   
-  validity = IsStationDataAvailable(station, station.type, start.date, end.date)
+  validity = IsStationDataAvailable(station, station.type, start_date, end_date)
   if (validity==0){
-    print(paste("Station data not available.", station, start.date, "to", "end.date"))
+    print(paste("Station data not available.", station, start_date, "to", "end_date"))
     return(0) #returning a NULL to signal no data
   }
   
-  date.range <- seq.Date(from=as.Date(start.date), to=as.Date(end.date), by='1 day')
+  date.range <- seq.Date(from=as.Date(start_date), to=as.Date(end_date), by='1 day')
   print(station)
   # pre-allocate list
   l <- vector(mode='list', length=length(date.range))
@@ -254,15 +281,15 @@ FetchStationWeatherForDateRange <- function(station, station.type, start.date, e
   # loop over dates, and fetch data
   for(i in seq_along(date.range))
   {
-    l[[i]] <- FetchDailyWeatherForStation(station, date.range[i], station.type)
-    print(paste(station, i, date.range[i], ":",  nrow(l[[i]]) ))
+    l[[i]] <- getSingleDayWeather(station,  date.range[i], station.type)
+    message(paste(station, i, date.range[i], ":",  nrow(l[[i]]) ))
   }
   
   #print(l)  
   # stack elements of list into DF, filling missing columns with NA
   d <- ldply(l)
     
-  outFileName <- paste0(station,"_",start.date,"_",end.date)
+  outFileName <- paste0(station,"_",start_date,"_",end_date)
   outFileName <- paste(outFileName, "csv","gz", sep=".")
   
   # save to CSV
